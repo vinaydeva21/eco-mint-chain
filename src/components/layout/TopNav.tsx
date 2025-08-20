1f607305da2c330893b3c82e1a774b8609a2033b
vinaydeva21 <136690122+vinaydeva21@users.noreply.github.com>
2025-08-20 14:01:56 +0530

    Update TopNav.tsx

--- src/components/layout/TopNav.tsx
+++ src/components/layout/TopNav.tsx
@@ -23,9 +23,9 @@ const TopNav: React.FC = () => {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-20 items-center justify-between px-4">
-        <Link to={"/"}>
-        <img src="/src/assets/streamline_logo.png" alt="Logo" className="h-32 sm:h-36 md:h-40 lg:h-44 xl:h-48" />
-        </Link>
+       // <Link to={"/"}>
+        //<img src="/src/assets/streamline_logo.png" alt="Logo" className="h-32 sm:h-36 md:h-40 lg:h-44 xl:h-48" />
+        // </Link>
         <nav className="hidden md:flex items-center gap-6 text-sm">
           {visibleItems.map((item) => (
             <NavLink
